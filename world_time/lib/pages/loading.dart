a443caf88304172a911df0045f6a14b808c940ee
android_shakil <shakil!@#$1234>
2021-06-13 17:22:51 +0600

    [Added] World Time API

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -10,18 +10,29 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData() async {
-    Response response = await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+  void getTime() async {
+
+    /** Make the request */
+    Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/Europe/London'));
     Map data = jsonDecode(response.body);
-    print(data);
-    print(data['title']);
+    //print(data);
+
+    /** Get properties from data */
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+    /*print(datetime);
+    print(offset);*/
+
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
   }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
