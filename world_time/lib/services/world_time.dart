c1f7ce51b574554135180468a34f14d648b40e1f
android_shakil <shakil!@#$1234>
2021-06-13 23:57:48 +0600

    [Update] Formatting & Showing Dates

--- world_time/lib/services/world_time.dart
+++ world_time/lib/services/world_time.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart';
+import 'package:intl/intl.dart';
 
 class WorldTime {
   late String location; /** Location name for the UI */
@@ -27,7 +28,7 @@ class WorldTime {
       now = now.add(Duration(hours: int.parse(offset)));
 
       /** Set the time property */
-      time = now.toString();
+      time = DateFormat.jm().format(now);
     }
     catch(e){
       print('caught error: $e');
