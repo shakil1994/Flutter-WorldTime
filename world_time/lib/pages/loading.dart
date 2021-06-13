b9490e31f87b2c337598a53e67ec92d9bf20606d
android_shakil <shakil!@#$1234>
2021-06-14 00:10:36 +0600

    [Added] Loaders / Spinners

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
@@ -30,9 +31,12 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50.0),
-        child: Text('loading'),
+      backgroundColor: Colors.blue[900],
+      body: Center(
+          child: SpinKitHourGlass(
+            color: Colors.white,
+            size: 50.0,
+          ),
       ),
     );
   }
