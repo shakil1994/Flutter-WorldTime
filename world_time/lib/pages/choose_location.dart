30be553c7db6bd123af804ba3a55841b19a2afa5
android_shakil <shakil!@#$1234>
2021-06-14 11:41:28 +0600

    [Added] Updating the time

--- world_time/lib/pages/choose_location.dart
+++ world_time/lib/pages/choose_location.dart
@@ -14,6 +14,19 @@ class _ChooseLocationState extends State<ChooseLocation> {
     WorldTime(location: 'Chicago', flag: 'usa.png', url: 'America/Chicago')
   ];
 
+  void updateTime(index) async {
+    WorldTime instance = locations[index];
+    await instance.getTime();
+
+    /** Navigate to home screen */
+    Navigator.pop(context, {
+      'location' : instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDayTime': instance.isDayTime
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,7 +45,7 @@ class _ChooseLocationState extends State<ChooseLocation> {
             child: Card(
               child: ListTile(
                 onTap: (){
-                  print(locations[index].location);
+                  updateTime(index);
                 },
                 title: Text(locations[index].location),
                 leading: CircleAvatar(
--- world_time/lib/pages/home.dart
+++ world_time/lib/pages/home.dart
@@ -10,8 +10,8 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    data = ModalRoute.of(context)!.settings.arguments as Map;
-    print(data);
+    data = data.isNotEmpty ? data : ModalRoute.of(context)!.settings.arguments as Map;
+    //print(data);
 
     /** Set Background */
     String bgImage = data['isDayTime'] ? 'day.png' : 'night.png';
@@ -31,8 +31,16 @@ class _HomeState extends State<Home> {
           child: Column(
             children: [
               FlatButton.icon(
-                  onPressed: () {
-                    Navigator.pushNamed(context, "/location");
+                  onPressed: () async {
+                    dynamic result = await Navigator.pushNamed(context, "/location");
+                    setState(() {
+                      data = {
+                        'time': result['time'],
+                        'location' : result['location'],
+                        'isDayTime': result['isDayTime'],
+                        'flag': result['time']
+                      };
+                    });
                   },
                   icon: Icon(
                     Icons.edit_location,
